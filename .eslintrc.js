c4c1971cb45c5180f8a8c68dc18be586d2a9affa
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-04-08 13:22:02 +0200

    add nest-cli generated base project
    build: fix linting and imports
    
    ci: add pnpm to pipelines
    
    ci: update pipelines

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  extends: [
+    'airbnb-typescript/base',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.spec.ts', '**/*.e2e-spec.ts'] },
+    ],
+  },
+};
