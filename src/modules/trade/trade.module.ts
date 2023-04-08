48ca5e6587d0fc29bb6cb47d8f322af2e3ba0e5c
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-04-08 21:43:27 +0200

    make resource service available to trade service

--- src/modules/trade/trade.module.ts
+++ src/modules/trade/trade.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TradeController } from '@/modules/trade/trade.controller';
+import { ResourceModule } from '@/modules/resource/resource.module';
 import { TradeService } from '@/modules/trade/trade.service';
 
 @Module({
-  imports: [],
+  imports: [ResourceModule],
   controllers: [TradeController],
   providers: [TradeService],
 })
--- src/modules/trade/trade.service.ts
+++ src/modules/trade/trade.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Resource } from '@/modules/resource/types';
+import { ResourceService } from '@/modules/resource/resource.service';
 
 @Injectable()
 export class TradeService {
-  getResource(
-    givenResourceName: string,
-    requestedResource: { name: string; amount: number },
-  ) {
-    console.log(`Got resource: ${givenResourceName}`);
-    return requestedResource;
+  constructor(private readonly resourceService: ResourceService) {}
+
+  getResource(type: Resource, amount: number) {
+    const receivedAmount = this.resourceService.takeAmountOfResource(
+      type,
+      amount,
+    );
+
+    return {
+      type: type,
+      amount: receivedAmount,
+    };
   }
 }
