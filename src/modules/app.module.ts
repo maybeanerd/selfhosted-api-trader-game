6031b54080d42370bcc9d3ca9825607b03b10a38
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-04-10 22:12:22 +0200

    add mongoose, but dont use it yet

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TradeModule } from '@/modules/trade/trade.module';
 import { ResourceModule } from '@/modules/resource/resource.module';
+// import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    // MongooseModule.forRoot('mongodb://localhost/nest'),
+
     TradeModule,
     ResourceModule,
   ],
