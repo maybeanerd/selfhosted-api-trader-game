01c50702fa2e4bd2928ac18f013e952f3915cac0
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-04-13 21:52:23 +0200

    fix db connection from dev mode to local DB container

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,11 +1,13 @@
 const dbUsername = process.env.DATABASE_USER;
 const dbPassword = process.env.DATABASE_PASSWORD;
+const dbHost = process.env.DATABASE_HOST;
 
-if (!dbUsername || !dbPassword) {
+if (!dbUsername || !dbPassword || !dbHost) {
   throw new Error('Missing database credentials from environment variables.');
 }
 
 export const dbConfig = {
   userName: dbUsername,
   password: dbPassword,
+  host: dbHost,
 };
