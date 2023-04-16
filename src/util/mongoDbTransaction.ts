174993cdc833ab96c156ab32ca4a925cf71ab265
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-04-16 17:01:09 +0200

    add function to delete existing trade offers
    defaulted to getting 80% of the offered resources back

--- src/util/mongoDbTransaction.ts
+++ src/util/mongoDbTransaction.ts
@@ -1,6 +1,16 @@
 import { ClientSession, Connection } from 'mongoose';
 
-export async function transaction<T>(connection: Connection, cb: (session: ClientSession) => Promise<T>): Promise<T> {
+export async function transaction<T>(
+  connection: Connection,
+  cb: (session: ClientSession) => Promise<T>,
+  passedSession?: ClientSession,
+): Promise<T> {
+  // We want to be able to chain transaction using functions within eachother,
+  // so when there is already a session passed we assume it's using a transaction and just continue with it.
+  if (passedSession) {
+    return cb(passedSession);
+  }
+
   const session = await connection.startSession();
 
   let result: T;
@@ -9,7 +19,7 @@ export async function transaction<T>(connection: Connection, cb: (session: Clien
   });
 
   await session.endSession();
-  
+
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   return result!;
 }
