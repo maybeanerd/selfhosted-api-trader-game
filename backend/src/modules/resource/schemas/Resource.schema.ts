35be2c537f75caffd96a95131457d9552f86593a
Sebastian Di Luzio <sebidiluzio@gmail.com>
2023-07-18 22:43:52 +0200

    Update Resource.schema.ts

--- backend/src/modules/resource/schemas/Resource.schema.ts
+++ backend/src/modules/resource/schemas/Resource.schema.ts
@@ -1,20 +1,23 @@
 import { Column, Model, Table, DataType } from 'sequelize-typescript';
 import { ResourceType } from '../types';
 
+const uniqueKey = 'ownerAndType';
+
 @Table
 export class Resource extends Model {
-  @Column({ allowNull: false, type: DataType.UUID  })
+  @Column({ allowNull: false, type: DataType.UUID, unique: uniqueKey })
     ownerId: string;
 
   @Column({ allowNull: false })
     amount: number;
 
-  @Column({ allowNull: false  })
+  @Column({ allowNull: false })
     upgradeLevel: number;
 
   @Column({
     allowNull: false,
     type: DataType.ENUM(...Object.values(ResourceType)),
+    unique: uniqueKey,
   })
     type: ResourceType;
 }
