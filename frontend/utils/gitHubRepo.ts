0550bc389d1762e164bbf47de39433735f9766a2
Sebastian Di Luzio <sebidiluzio@gmail.com>
2024-03-10 12:30:23 +0100

    fix: github username

--- frontend/utils/gitHubRepo.ts
+++ frontend/utils/gitHubRepo.ts
@@ -1,5 +1,5 @@
 export const gitHubProjectUrl = new URL(
-  'https://github.com/T0TProduction/selfhosted-api-trader-game',
+  'https://github.com/maybeanerd/selfhosted-api-trader-game',
 );
 
 export function getLinkToCommit (commitHash: string) {
