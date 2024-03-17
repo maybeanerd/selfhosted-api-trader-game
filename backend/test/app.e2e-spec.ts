4732d17b5f2ca67dba75bff77ac5b5d098f7c48d
Sebastian Di Luzio <sebidiluzio@gmail.com>
2024-03-17 14:59:58 +0100

    Update app.e2e-spec.ts

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable drizzle/enforce-delete-with-where */
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
