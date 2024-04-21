6f01d17237e87a81524924ccacbc9267e9bdcee7
Sebastian Di Luzio <sebastian@diluz.io>
2024-04-21 17:01:11 +0200

    use the newly created controller and service

--- backend/src/modules/crossroads/crossroads.module.ts
+++ backend/src/modules/crossroads/crossroads.module.ts
@@ -5,11 +5,13 @@ import { EventService } from './event/event.service';
 import { HttpModule } from '@nestjs/axios';
 import { TradeModule } from '@/modules/trade/trade.module';
 import { TreatyModule } from '@/modules/treaty/treaty.module';
+import { ActivityPubService } from '@/modules/crossroads/activitypub/activitypub.service';
+import { ActivityPubController } from '@/modules/crossroads/activitypub/activitypub.controller';
 
 @Module({
   imports: [HttpModule, TreatyModule, TradeModule],
 
-  controllers: [TreatyController, EventController],
-  providers: [EventService],
+  controllers: [TreatyController, EventController, ActivityPubController],
+  providers: [EventService, ActivityPubService],
 })
 export class CrossroadsModule {}
