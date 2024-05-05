09884815ad310f45c27d1631c9bb1b29be713af9
Sebastian Di Luzio <sebastian@diluz.io>
2024-05-05 17:24:56 +0200

    feat: add UI to propose a treaty to other instances

---
+++ frontend/components/ProposeTreaty.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <UInput v-model="instanceUrl" class="p-2" />
+    <UButton class="p-2" @click="submitTreaty">
+      Submit Treaty
+    </UButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+const instanceUrl = ref('');
+
+async function submitTreaty() {
+  const treaty = {
+    url: instanceUrl.value,
+  };
+
+  await fetch('http://localhost:8080/v1/treaty', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(treaty),
+  });
+
+  instanceUrl.value = '';
+}
+
+</script>
--- frontend/pages/treaties.vue
+++ frontend/pages/treaties.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
+    <h1>Propose a Treaty:</h1>
+    <br>
+    <ProposeTreaty />
+    <br>
     <h1>Available Treaties:</h1>
+    <br>
     <ShowAllTreaties />
   </div>
 </template>
