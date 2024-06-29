b4c7b196a89d56c3e64d933d20fc3ac613f057c7
Sebastian Di Luzio <sebastian@diluz.io>
2024-06-29 16:16:03 +0200

    feat(backend): add icon and summary for fediverse actors
    the image is added to the repo itself so that we can use the github url as source URL of the icon/image for all actors - no need to build our own API endpoint for this, yet

--- backend/src/modules/crossroads/activitypub/actor/index.ts
+++ backend/src/modules/crossroads/activitypub/actor/index.ts
@@ -10,8 +10,17 @@ import {
   getInboxUrl,
   getOutboxUrl,
 } from '@/modules/crossroads/activitypub/utils/apUrl';
+import { serverInfo } from '@/config/serverInfo';
 import { activityPubIsGameServerExtension } from '@/modules/crossroads/activitypub/utils/gameServerExtension';
 import { drizz } from 'db';
+
+const gitHubProjectUrl = new URL(
+  'https://github.com/maybeanerd/selfhosted-api-trader-game',
+);
+
+function getHtmlLink(url: URL) {
+  return `<a href="${url.toString()}">${url.toString()}</a>`;
+}
 
 async function getActorFromId(
   { id, publicKey }: { id: string; publicKey: string },
@@ -30,6 +39,17 @@ async function getActorFromId(
     id: actorId,
     type,
     preferredUsername: username ?? id,
+    summary: `Actor in charge of handling trades between players for the game server ${getHtmlLink(serverInfo.baseUrl)}.
+For more information about the game itself, check out ${getHtmlLink(gitHubProjectUrl)}.`,
+    icon: {
+      type: 'Image',
+      mediaType: 'image/png',
+      url:
+        gitHubProjectUrl.toString() +
+        '/blob/main/backend/src/modules/crossroads/activitypub/assets/merchant-profile-picture.png',
+    },
+    // image: ...  // Banner image, if we wanted it
+
     inbox: getInboxUrl().toString(),
     outbox: getOutboxUrl().toString(),
     followers: getFollowersUrl().toString(),
